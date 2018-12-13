eadf24ea91488a917f2503345e87b40c5825f4e5
Pedro Ferreira <pedro.ferreira@cern.ch>
2018-12-13 13:59:37 +0100

    Add CLI script

--- setup.py
+++ setup.py
@@ -1,14 +1,20 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='indico-mattermost-bot',
     version='0.1',
     license='ASL 2.0',
     long_description=open('README.md').read(),
+    packages=find_packages(),
     install_requires=[
         'click',
         'pytz',
         'requests',
         'configparser'
-    ]
+    ],
+    entry_points={
+        'console_scripts': [
+            'indico-mm-bot=indico_mattermost_bot.bot:cli'
+        ]
+    }
 )
