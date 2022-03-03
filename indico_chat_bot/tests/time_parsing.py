94f6ad6a40b6ca1beab4d4980abeb098ba1c851e
nico <nicolas.rodriguezcuellar@student.hu.nl>
2022-03-03 12:50:48 +0100

    More specific condition, custom exceptions and start on unittest
    I added a specific check on the event start_time delta to be more explicit about when an event should be notified. I added custom exceptions that can be reused. And a small unittest.

---
+++ indico_chat_bot/tests/time_parsing.py
@@ -0,0 +1,27 @@
+import unittest
+from indico_chat_bot.bot import _parse_time_delta
+from datetime import timedelta
+
+
+class TimeParsingTests(unittest.TestCase):
+    def test_time_delta_parsing(self):
+        examples = {
+            'minutes': '30m',
+            'hours': '2h',
+            'days': '7d',
+        }
+
+        expected = {
+            'minutes': timedelta(minutes=30),
+            'hours': timedelta(hours=2),
+            'days': timedelta(days=7),
+        }
+
+        for time_unit, delta in examples.items():
+            parsed_delta = _parse_time_delta(delta)
+            expected_result = expected[time_unit]
+            self.assertEqual(parsed_delta, expected_result)
+
+
+if __name__ == '__main__':
+    unittest.main()
